96f12f96c710ba48b404b83bdacd20b93c25c117
Melisha <mslafaber1@gmail.com>
2020-08-05 18:04:04 +0930

    edit part 3 on the week 2 ef

---
+++ HelloWorldMVCwebApp/Migrations/20200805051208_MovieClasswithCategoryName.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HelloWorldMVCwebApp.Migrations
+{
+    public partial class MovieClasswithCategoryName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryNameCategoryId",
+                table: "Movies",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_CategoryNameCategoryId",
+                table: "Movies",
+                column: "CategoryNameCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Categories_CategoryNameCategoryId",
+                table: "Movies",
+                column: "CategoryNameCategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Categories_CategoryNameCategoryId",
+                table: "Movies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_CategoryNameCategoryId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryNameCategoryId",
+                table: "Movies");
+        }
+    }
+}
